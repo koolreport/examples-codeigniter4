d73380fe4d0644acc5c9d4d86fa147471215a6fa
dongnl <nguyenluongdong@gmail.com>
2025-06-20 15:29:30 +0700

    update root url in common

--- public/helpers/common.php
+++ public/helpers/common.php
@@ -21,6 +21,38 @@ function getRootUrl()
     return $root_url;
 }
 
+function getScriptFile()
+{
+    if (isset($_SERVER['SCRIPT_FILENAME'])) {
+        return $_SERVER['SCRIPT_FILENAME'];
+    }
+
+    throw new \Exception('Unable to determine the entry script file path.');
+}
+
+function getScriptUrl()
+{
+    $scriptFile = getScriptFile();
+    $scriptName = basename($scriptFile);
+    if (isset($_SERVER['SCRIPT_NAME']) && basename($_SERVER['SCRIPT_NAME']) === $scriptName) {
+        $scriptUrl = $_SERVER['SCRIPT_NAME'];
+    } elseif (isset($_SERVER['PHP_SELF']) && basename($_SERVER['PHP_SELF']) === $scriptName) {
+        $scriptUrl = $_SERVER['PHP_SELF'];
+    } elseif (isset($_SERVER['ORIG_SCRIPT_NAME']) && basename($_SERVER['ORIG_SCRIPT_NAME']) === $scriptName) {
+        $scriptUrl = $_SERVER['ORIG_SCRIPT_NAME'];
+    } elseif (isset($_SERVER['PHP_SELF']) && ($pos = strpos($_SERVER['PHP_SELF'], '/' . $scriptName)) !== false) {
+        $scriptUrl = substr($_SERVER['SCRIPT_NAME'], 0, $pos) . '/' . $scriptName;
+    } elseif (!empty($_SERVER['DOCUMENT_ROOT']) && strpos($scriptFile, $_SERVER['DOCUMENT_ROOT']) === 0) {
+        $scriptUrl = str_replace([$_SERVER['DOCUMENT_ROOT'], '\\'], ['', '/'], $scriptFile);
+    } else {
+        throw new \Exception('Unable to determine the entry script URL.');
+    }
+
+    return $scriptUrl;
+}
+// echo getScriptUrl() . "<br>";
 $menu = json_decode(file_get_contents(realpath(dirname(__FILE__))."/../reports.json"),true);
 // $root_url = getRootUrl();
-$root_url = base_url();
+$root_url = rtrim(dirname(getScriptUrl()), '\\/');
+// $root_url = base_url();
+// echo "root_url: $root_url<br>";
